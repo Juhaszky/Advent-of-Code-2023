a1986c9a6db583526f12b0a16d4af5b98ca5c24f
Juhaszky <juhasz.krisztian2000@gmail.com>
2024-01-03 19:29:41 +0100

    Day-01 solving function implemented

--- Day-01/index.ts
+++ Day-01/index.ts
@@ -5,25 +5,54 @@ function main(): void {
   const answer = processLine(input);
   console.log(answer);
 }
+
 function readInput(): string[] {
   const words = fs.readFileSync("./input.txt", "utf-8");
   return words.split("\n");
 }
+
 function processLine(words: string[]): number {
   let calibrationValueSum = 0;
-  words.forEach((line) => {
-    const numberString = line.match(/\d+/g)?.join("");
-                if (numberString) {
-        if (numberString.length === 1) {
-        calibrationValueSum += +(numberString[0] + numberString[0]);
+  words.forEach((word) => {
+    word = transformLetterNumbersToNumbers(word);
+    const numberString = word.match(/\d+/g)?.join("");
+    if (numberString) {
+      if (numberString.length === 1) {
+        calibrationValueSum += (+(numberString[0] + numberString[0]));
       } else {
-        calibrationValueSum += +(
-          numberString[0] + numberString[numberString.length - 1]
-        );
+        calibrationValueSum += (+(numberString[0] + numberString[numberString.length - 1]));
       }
     }
   });
+  console.log('calSum: ', calibrationValueSum);
   return calibrationValueSum;
 }
+
+function transformLetterNumbersToNumbers(word: string): string {
+  const textToNumberMapping: Record<string, string> = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+  };
+
+  const pattern = new RegExp(Object.keys(textToNumberMapping).join('|'), 'g');
+  let match = word.match(pattern);
+
+  while (match) { 
+    match.forEach((m) => {
+      word = word.replace(m, textToNumberMapping[m] + m[m.length - 1]);
+    });
+    match = word.match(pattern);
+  }
+
+  return word;
+}
+
 
 main();
